d9f554a7bcd93234adbc49c136e2f8dfa106e6d9
Leslie Shea <leslieyshea@gmail.com>
2019-04-11 10:29:00 -0700

    added period to text

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ function start(route, handle) {
   }
   http.createServer(onRequest).listen(8888);
   /*eslint-disable-next-line*/
-  console.log('Server has started');
+  console.log('Server has started.');
 }
 
 exports.start = start;
