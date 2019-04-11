c3a189088e4ccd626205324bead0a474e1751b45
Leslie Shea <leslieyshea@gmail.com>
2019-04-11 10:36:30 -0700

    added blocking operation

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,11 +4,20 @@
 function start() {
   /*eslint-disable-next-line*/
   console.log('Request handler "start" was called.');
+
+  function sleep(milliSeconds) {
+    const startTime = new Date().getTime();
+    while(new Date.getTime() < startTime + milliSeconds);
+  }
+
+  sleep(10000);
+  return 'Hello Start';
 }
 
 function upload() {
   /*eslint-disable-next-line*/
   console.log('Request handler "upload" was called.');
+  return 'Hello Upload';
 }
 
 exports.start = start;
