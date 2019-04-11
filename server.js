1cb5220f140d52a8af62783f5105137f5f8a13f2
Leslie Shea <leslieyshea@gmail.com>
2019-04-11 10:10:40 -0700

    added url pathname

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const url = require('url');
 
 function start() {
   function onRequest(request, response) {
-    console.log('Request received.');
+    const pathname = url.parse(request.url).pathname;
+    /*eslint-disable-next-line*/
+    console.log('Request for ' + pathname + 'received.');
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write('Hello World');
     response.end();
