3f5acf9153e89164d95885ea031c8ac0e40089bc
Leslie Shea <leslieyshea@gmail.com>
2019-04-12 15:36:14 -0700

    updated html

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,5 @@
 const querystring = require('querystring');
+const fs = require('fs');
 
 function start(response, postData) {
   /*eslint-disable-next-line*/
@@ -6,18 +7,18 @@ function start(response, postData) {
 
   const body = '<html>' +
     '<head>' +
-    '<meta http-equiv="Content-Type" content="text/html; '+
-    'charset=UTF-8" />' +
+    '<meta http-equiv="Content-Type" ' + 
+    'content="text/html; charset=UTF-8" />' +
     '</head>' +
     '<body>' +
     '<form action="/upload" method="post">' +
-    'textarea name="text" rows="20" cols="60"><textarea>' +
+    '<textarea name="text" rows="20" cols="60"></textarea>' +
     '<input type="submit" value="Submit text" />' +
     '</form>' +
     '</body>' +
     '</html>';
 
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(body);
   response.end();
 }
@@ -26,9 +27,17 @@ function upload(response, postData) {
   /*eslint-disable-next-line*/
   console.log('Request handler "upload" was called.');
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('You\'ve sent: ' + querystring.parse(postData).text);
+  response.write('You\'ve sent the text: ' + querystring.parse(postData).text);
   response.end();
+}
+
+function show(response) {
+  /*eslint-disable-next-line*/
+  console.log('Request handler "show" was called.');
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  fs.createReadStream('/tmp/test.png').pipe(response);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
