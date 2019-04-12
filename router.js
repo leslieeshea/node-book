e0e5b4db09e5b209ecd27ba10a54c8c971f5002d
Leslie Shea <leslieyshea@gmail.com>
2019-04-12 13:58:54 -0700

    display the received content

--- router.js
+++ router.js
@@ -1,8 +1,9 @@
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
   /*eslint-disable-next-line*/
   console.log('About to route a request for ' + pathname);
+
   if(typeof handle[pathname] === 'function') {
-    return handle[pathname](response);
+    handle[pathname](response, postData);
   } else {
     /*eslint-disable-next-line*/
     console.log('No request handler found for ' + pathname);
