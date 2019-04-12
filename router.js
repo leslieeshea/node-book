05786a161d2372858e9c8760b66460143f8e469f
Leslie Shea <leslieyshea@gmail.com>
2019-04-12 15:43:13 -0700

    removed postData and passed in request

--- router.js
+++ router.js
@@ -1,13 +1,13 @@
-function route(handle, pathname, response, postData) {
+function route(handle, pathname, response, request) {
   /*eslint-disable-next-line*/
   console.log('About to route a request for ' + pathname);
 
   if(typeof handle[pathname] === 'function') {
-    handle[pathname](response, postData);
+    handle[pathname](response, request);
   } else {
     /*eslint-disable-next-line*/
     console.log('No request handler found for ' + pathname);
-    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.writeHead(404, { 'Content-Type': 'text/html' });
     response.write('404 Not found');
     response.end();
   }
