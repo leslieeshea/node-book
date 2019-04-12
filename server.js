c7c2ee92d2699c309247505f2c63a34cc7087cdc
Leslie Shea <leslieyshea@gmail.com>
2019-04-12 15:42:04 -0700

    removed request listeners

--- server.js
+++ server.js
@@ -3,21 +3,10 @@ const url = require('url');
 
 function start(route, handle) {
   function onRequest(request, response) {
-    let postData = '';
     const pathname = url.parse(request.url).pathname;
     /*eslint-disable-next-line*/
     console.log('Request for ' + pathname + ' received.');
-
-    request.setEncoding('utf8');
-
-    request.addListener('data', function(postDataChunk) {
-      postData += postDataChunk;
-      /*eslint-disable-next-line*/
-      console.log('Received POST data chunk ' + postDataChunk + '.');
-    });
-    request.addListener('end', function() {
-      route(handle, pathname, response, postData);
-    });
+    route(handle, pathname, response, request);
   }
   
   http.createServer(onRequest).listen(8888);
